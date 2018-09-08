bccb1e8a1b1b99be85ad1cb1de821a8a88d67c90
RubyG <rubyrgill@hotmail.com>
2018-09-08 11:33:20 -0400

    Input option for heroku

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var path = require("path");
 
 var app = express()
 
+//var PORT = process.env.PORT || 5000;
 var PORT = 5000;
 
-//var PORT = process.env.PORT || 5000;
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
